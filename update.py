a2fa9e17bb5b7e11556084f008852b5fb8420734
volatile rye <volatilerye@gmail.com>
2025-02-07 22:48:26 +0900

    fix: ⚡ skip non-fixed files update

--- update.py
+++ update.py
@@ -7,6 +7,8 @@ import markdown
 import os
 import re
 import requests
+import subprocess
+
 
 DEFAULT_GITHUB_ICONS = {
     "note": '<svg class="octicon octicon-info mr-2" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Zm8-6.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13ZM6.5 7.75A.75.75 0 0 1 7.25 7h1a.75.75 0 0 1 .75.75v2.75h.25a.75.75 0 0 1 0 1.5h-2a.75.75 0 0 1 0-1.5h.25v-2h-.25a.75.75 0 0 1-.75-.75ZM8 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"></path></svg>',
@@ -239,7 +241,6 @@ def generate_html_file(md_path: str):
             html_text = re.sub(":back_text:", "[top]", html_text)
 
         dir_depth = len(md_dir_list)
-        print(md_dir_list)
         if dir_depth == 1:
             contents_info = f"index"
         else:
@@ -279,9 +280,38 @@ def generate_html_file(md_path: str):
         f.write(html_text)
 
 
-for p in glob.glob("*/**/*.html", recursive=True):
+# ------
+
+
+def get_changed_files():
+    # Gitコマンドで変更のあるファイルを取得する
+    result = subprocess.run(
+        ["git", "status", "--porcelain"], capture_output=True, text=True
+    )
+
+    # 結果の出力からファイルパスのみ抽出
+    changed_files = []
+    for line in result.stdout.splitlines():
+        _, file_path = line[:2].strip(), line[3:].strip()
+        changed_files.append(file_path)
+
+    return changed_files
+
+
+markdown_files = [file for file in get_changed_files() if re.fullmatch(r".+\.md", file)]
+html_files = [re.sub(".md", ".html", file) for file in markdown_files]
+
+print(f"remove the following html files:\n{html_files}")
+
+for p in tqdm.tqdm(html_files):
     if os.path.isfile(p):
         os.remove(p)
+else:
+    print("🗑️ DONE: removed files.")
 
-for md in tqdm.tqdm(glob.glob("**/*.md", recursive=True)):
+print(f"generating html files from the following markdown files:\n{markdown_files}")
+
+for md in tqdm.tqdm(markdown_files):
     generate_html_file(md)
+else:
+    print("✅ DONE: generated html files.")
